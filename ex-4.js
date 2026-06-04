1eb2219ea9b1e72b33a98b12ec53f962e9b4677b
sukritta5447 <75234379+sukritta5447@users.noreply.github.com>
2026-06-04 15:00:40 +0700

    refactor: update exercises with variable initialization and console logs

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,17 @@ const employees = [
 ];
 
 // Start coding here
+const newEmployee1 = {
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"]
+};
+const newEmployee2 = {
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"]
+};
+
+employees.push(newEmployee1, newEmployee2);
+
+console.log(employees);
